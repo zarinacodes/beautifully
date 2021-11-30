eb720319a48f0d96a4caeb594874ef258718ede1
Zarina Tumaszewicz <zarinatumashevich@gmail.com>
2021-11-30 22:35:26 +0100

    Change in static files serving

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 const app = require("./config/express");
+const path = require("path");
 const PORT = process.env.PORT || 5000;
 
 app.use(express.static(path.resolve(__dirname, "../client/build")));
--- server/routes.js
+++ server/routes.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const api = require("./api/api.routes");
-const path = require("path");
 
 router.use("/api", api);
 
