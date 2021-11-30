ccac9037de35ba01bc7b5e3d314943ade9e038ce
Zarina Tumaszewicz <zarinatumashevich@gmail.com>
2021-11-30 22:24:40 +0100

    Change static files serving

--- server/index.js
+++ server/index.js
@@ -1,6 +1,11 @@
 const app = require("./config/express");
 const PORT = process.env.PORT || 5000;
 
+app.use(
+  "*",
+  express.static(path.resolve(__dirname, "../client/build", "index.html"))
+);
+
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
 });
