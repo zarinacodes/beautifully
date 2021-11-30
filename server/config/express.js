b42abec83370f8a6a71531903ed1446fd46bae70
Zarina Tumaszewicz <zarinatumashevich@gmail.com>
2021-11-30 22:30:12 +0100

    Change static files serving

--- server/config/express.js
+++ server/config/express.js
@@ -2,11 +2,15 @@ const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
 const routes = require("../routes");
+const path = require("path");
 const app = express();
 
 app.use(express.json());
 app.use(helmet());
 app.use(cors());
 app.use("/", routes);
+app.use(
+  express.static(path.resolve(__dirname, "../client/build", "index.html"))
+);
 
 module.exports = app;
--- server/routes.js
+++ server/routes.js
@@ -5,4 +5,8 @@ const path = require("path");
 
 router.use("/api", api);
 
+router.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
+});
+
 module.exports = router;
